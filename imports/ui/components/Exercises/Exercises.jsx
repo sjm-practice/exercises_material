f12ccd87275412d8758479428db6ffe1f055df90
stevenjmarsh <sjm.atl@gmail.com>
2018-07-15 23:19:08 -0400

    listing exercises and welcome message in panes

--- imports/ui/components/Exercises/Exercises.jsx
+++ imports/ui/components/Exercises/Exercises.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const styles = {
-  Paper: { padding: 20, marginTop: 10, marginBottom: 10 },
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto",
+  },
 };
 
-const Exercises = () => (
+const Exercises = ({ exercises }) => (
   <Grid container>
     <Grid item sm>
-      <Paper styles={styles.Paper}>Left Pane</Paper>
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) => (
+          <Fragment key={group}>
+            <Typography variant="headline" style={{ textTransform: "capitalize" }}>
+              {group}
+            </Typography>
+            <List component="nav">
+              {exercises.map(({ title }) => (
+                <ListItem key={title} button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              ))}
+            </List>
+          </Fragment>
+        ))}
+      </Paper>
     </Grid>
     <Grid item sm>
-      <Paper styles={styles.Paper}>Rigth Pane</Paper>
+      <Paper style={styles.Paper}>
+        <Typography variant="display1">Welcome!</Typography>
+        <Typography variant="subheading" style={{ marginTop: 20 }}>
+          Please select an exercise from the list on the left.
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
 );
