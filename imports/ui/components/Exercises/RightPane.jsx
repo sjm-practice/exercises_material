43a36dd64c516d6fa0d8e52ef1e41f21d041a5df
stevenjmarsh <sjm.atl@gmail.com>
2018-07-15 21:49:51 -0400

    correcting typo, left to right pane

--- imports/ui/components/Exercises/RightPane.jsx
+++ imports/ui/components/Exercises/RightPane.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
 
-const RightPane = ({ styles }) => <Paper style={styles.Paper}>Left Pane</Paper>;
+const RightPane = ({ styles }) => <Paper style={styles.Paper}>Right Pane</Paper>;
 
 RightPane.propTypes = {
   styles: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
